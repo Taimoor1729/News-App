299798fd98151bf5a5fb2a76ad4cb8a93b206cc2
taimoor1729 <temitech669@gmail.com>
2023-06-18 23:25:17 +0500

    news item are not displaying

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -8,13 +8,13 @@ export default class NewsItem extends Component {
         return (
           <>
             <div className="card my-3 " style={{width : "18rem"}}>
-              <img className="card-img-top" src={imageUrl} alt="Card image cap" />
+              <img className="card-img-top" src={imageUrl} alt="Card cap" />
               <div className="card-body">
                 <h5 className="card-title">{title}</h5>
                 <p className="card-text">
                  {description}
                 </p>
-                <a href={newsUrl} target="_blank" className="btn btn-sm btn-primary">
+                <a href={newsUrl} target="_blank" rel="noreferrer" className="btn btn-sm btn-primary" >
                   Go somewhere
                 </a>
               </div>
