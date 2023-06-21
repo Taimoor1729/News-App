d65f5e0dd4fdb467da11611dadcac669b927e15f
taimoor1729 <temitech669@gmail.com>
2023-06-21 23:08:44 +0500

    minor changes on navbar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import Navbar from './components/Navbar'
+import Navbar from './components/navbar/Navbar'
 import News from './components/News'
 export default class App extends Component {
   render() {
